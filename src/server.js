7aa935f9976a28b1ea82dbc2259b277a14613691
Tomas Juarez <mokajua@gmail.com>
2023-02-11 14:45:07 -0300

    port

--- src/server.js
+++ src/server.js
@@ -7,9 +7,8 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 app.use(require("./routes/send-email"))
-
 app.use(express.static(path.join(__dirname,"public")))
 
 app.listen(PORT,()=>{
-    console.log("server listening on port 3000")
+    console.log(`server listening on port ${PORT}`)
 })
